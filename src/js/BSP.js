26d0b7c116c51f78a1624220966b46f3dd77300a
71104 <a71104@gmail.com>
2017-07-15 22:28:05 +0100

    Clusters

--- src/js/BSP.js
+++ src/js/BSP.js
@@ -1,24 +1,65 @@
 Quake2.BSP = function (gl, data) {
-  this._gl = gl;
-  this._clusterMap = Object.create(null);
-  const root = this._parse(data, 0);
-  this._setPVS(data);
-  return root;
+  this._clusters = Object.create(null);
+  this._createClusters(gl, data, 0);
+  this._pvs = Quake2.PVS.parse(data);
+  return this._parse(data, 0);
+};
+
+Quake2.BSP.prototype._createClusters = function (gl, data, index) {
+  if (index < 0) {
+    const clusterIndex = data.leaves.cluster[-(index + 1)];
+    if (clusterIndex >= 0 && !this._clusters[clusterIndex]) {
+      this._clusters[clusterIndex] = Quake2.Cluster.load(gl, data, clusterIndex);
+    }
+  } else {
+    this._createClusters(gl, data, data.nodes.front[index]);
+    this._createClusters(gl, data, data.nodes.back[index]);
+  }
 };
 
 Quake2.BSP.prototype._parse = function (data, index) {
   if (index < 0) {
-    return new Quake2.BSP.Leaf(this._gl, data, -(index + 1), this._clusterMap);
+    const leafIndex = -(index + 1);
+    const clusterIndex = data.leaves.cluster[leafIndex];
+    var clusters = clusterIndex < 0 ? [] : this._pvs[clusterIndex].map(function (i) {
+      return this._clusters[i];
+    }, this);
+    return new Quake2.BSP.Leaf(data, leafIndex, clusters);
   } else {
     return new Quake2.BSP.Node(this, data, index);
   }
 };
 
-Quake2.BSP.prototype._setPVS = function (data) {
-  const pvs = Quake2.PVS.parse(data);
-  for (var i in this._clusterMap) {
-    this._clusterMap[i]._setPVS(pvs[i].map(function (j) {
-      return this._clusterMap[j];
-    }, this));
+
+Quake2.BSP.Node = function (bsp, data, index) {
+  const planeIndex = data.nodes.plane[index];
+  this.plane = data.planes.slice(planeIndex * 4, (planeIndex + 1) * 4);
+  this.front = bsp._parse(data, data.nodes.front[index]);
+  this.back = bsp._parse(data, data.nodes.back[index]);
+};
+
+Quake2.BSP.Node.prototype.locate = function (position) {
+  const x = position.x * this.plane[0] + position.y * this.plane[1] +
+      position.z * this.plane[2] - this.plane[3];
+  if (x < 0) {
+    return this.back.locate(position);
+  } else {
+    return this.front.locate(position);
+  }
+};
+
+
+Quake2.BSP.Leaf = function (data, index, clusters) {
+  this.clusters = clusters;
+  // TODO: add planes
+};
+
+Quake2.BSP.Leaf.prototype.locate = function () {
+  return this;
+};
+
+Quake2.BSP.Leaf.prototype.render = function () {
+  for (var i = 0; i < this.clusters.length; i++) {
+    this.clusters[i].render();
   }
 };
---
+++ src/js/Cluster.js
@@ -0,0 +1,127 @@
+Quake2.EmptyCluster = function () {};
+
+Quake2.EmptyCluster.prototype.render = function () {};
+
+
+Quake2.Cluster = function (gl, data, index, size) {
+  this._gl = gl;
+
+  this._vertexBuffer = gl.createBuffer();
+  this._normalBuffer = gl.createBuffer();
+  this._textureCoordinateBuffer = gl.createBuffer();
+  this._textureOriginBuffer = gl.createBuffer();
+  this._textureSizeBuffer = gl.createBuffer();
+
+  var vertices = [];
+  var normals = [];
+  var textureCoordinates = [];
+  var textureOrigins = [];
+  var textureSizes = [];
+
+  const _pushElement = function (vertexIndex, normalIndex, textureIndex) {
+    vertices.push(
+        data.vertices[vertexIndex * 3],
+        data.vertices[vertexIndex * 3 + 1],
+        data.vertices[vertexIndex * 3 + 2]);
+    if (normalIndex < 0) {
+      normals.push(
+          -data.planes[-normalIndex * 4],
+          -data.planes[-normalIndex * 4 + 1],
+          -data.planes[-normalIndex * 4 + 2]);
+    } else {
+      normals.push(
+          data.planes[normalIndex * 4],
+          data.planes[normalIndex * 4 + 1],
+          data.planes[normalIndex * 4 + 2]);
+    }
+    textureCoordinates.push(
+        data.vertices[vertexIndex * 3 + 0] * data.textureInformation.u[textureIndex * 4 + 0] +
+        data.vertices[vertexIndex * 3 + 1] * data.textureInformation.u[textureIndex * 4 + 1] +
+        data.vertices[vertexIndex * 3 + 2] * data.textureInformation.u[textureIndex * 4 + 2] +
+        data.textureInformation.u[textureIndex * 4 + 3]);
+    textureCoordinates.push(
+        data.vertices[vertexIndex * 3 + 0] * data.textureInformation.v[textureIndex * 4 + 0] +
+        data.vertices[vertexIndex * 3 + 1] * data.textureInformation.v[textureIndex * 4 + 1] +
+        data.vertices[vertexIndex * 3 + 2] * data.textureInformation.v[textureIndex * 4 + 2] +
+        data.textureInformation.v[textureIndex * 4 + 3]);
+    textureOrigins.push(
+        data.textureInformation.x[textureIndex],
+        data.textureInformation.y[textureIndex]);
+    textureSizes.push(
+        data.textureInformation.w[textureIndex],
+        data.textureInformation.h[textureIndex]);
+  };
+
+  const _pushFace = function (i) {
+    var k0 = data.faceEdges[data.faces.edges.offset[i]];
+    if (k0 < 0) {
+      k0 = data.edges[-k0 * 2 + 1];
+    } else {
+      k0 = data.edges[k0 * 2];
+    }
+    for (var j = 1; j < data.faces.edges.size[i] - 1; j++) {
+      _pushElement(k0, data.faces.planes[i], data.faces.textureInformation[i]);
+      const k = data.faceEdges[data.faces.edges.offset[i] + j];
+      if (k < 0) {
+        _pushElement(data.edges[-k * 2 + 1], data.faces.planes[i], data.faces.textureInformation[i]);
+        _pushElement(data.edges[-k * 2], data.faces.planes[i], data.faces.textureInformation[i]);
+      } else {
+        _pushElement(data.edges[k * 2], data.faces.planes[i], data.faces.textureInformation[i]);
+        _pushElement(data.edges[k * 2 + 1], data.faces.planes[i], data.faces.textureInformation[i]);
+      }
+    }
+  };
+
+  for (var i = 0; i < data.leaves.faces.count[index]; i++) {
+    _pushFace(data.leaves.faces.table[data.leaves.faces.first[index] + i]);
+  }
+
+  this.size = vertices.length / 3;
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this._vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this._normalBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this._textureCoordinateBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates), gl.STATIC_DRAW);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this._textureOriginBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureOrigins), gl.STATIC_DRAW);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this._textureSizeBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureSizes), gl.STATIC_DRAW);
+
+};
+
+
+Quake2.Cluster.prototype.render = function () {
+  const gl = this._gl;
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this._vertexBuffer);
+  gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this._normalBuffer);
+  gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 0, 0);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this._textureCoordinateBuffer);
+  gl.vertexAttribPointer(2, 2, gl.FLOAT, false, 0, 0);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this._textureOriginBuffer);
+  gl.vertexAttribPointer(3, 2, gl.FLOAT, false, 0, 0);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, this._textureSizeBuffer);
+  gl.vertexAttribPointer(4, 2, gl.FLOAT, false, 0, 0);
+
+  gl.drawArrays(gl.TRIANGLES, 0, this._size);
+};
+
+
+Quake2.Cluster.load = function (gl, data, index) {
+  if (data.leaves.faces.count[index] > 0) {
+    return new Quake2.Cluster(gl, data, index);
+  } else {
+    return new Quake2.EmptyCluster();
+  }
+};
