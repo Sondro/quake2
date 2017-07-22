e2481eb401015c29328e94d4326b8745b63222c7
71104 <a71104@gmail.com>
2017-07-22 15:01:27 +0100

    Array.prototype.unique()

--- src/js/Quake2.js
+++ src/js/Quake2.js
@@ -4,3 +4,28 @@ Quake2.inherit = function (Base, Derived) {
   Derived.prototype = Object.create(Base.prototype);
   Derived.prototype.constructor = Derived;
 };
+
+Array.prototype.unique = function () {
+  const sorted = this.slice().sort(function (a, b) {
+    if (a < b) {
+      return -1;
+    } else if (a > b) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+  if (sorted.length) {
+    const result = [];
+    var last = sorted[0];
+    for (var i = 1; i < sorted.length; i++) {
+      if (sorted[i] !== last) {
+        last = sorted[i];
+        result.push(last);
+      }
+    }
+    return result;
+  } else {
+    return [];
+  }
+};
