218d182f1bff740035f1d4ed656a6e8e6083f151
71104 <a71104@gmail.com>
2017-08-02 14:14:18 +0100

    Minifying texture filter still looks good with nearest.

--- src/js/WorldProgram.js
+++ src/js/WorldProgram.js
@@ -9,7 +9,7 @@ Quake2.WorldProgram = function (gl, assets, camera) {
 
   gl.activeTexture(gl.TEXTURE0);
   gl.bindTexture(gl.TEXTURE_2D, this._textures.atlas);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, assets.texture);
 
