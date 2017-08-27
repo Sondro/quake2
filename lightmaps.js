a7cc29e9f1dc6bb81b15e910736556de1839dbe8
71104 <a71104@gmail.com>
2017-08-27 18:37:37 +0100

    Refactoring

--- lightmaps.js
+++ lightmaps.js
@@ -1,10 +1,31 @@
-var lightmap = require('./lightmap.js');
+var lightmap2png = require('./lightmap2png.js');
 var atlas = require('./atlas.js');
 
 
 Array.prototype.flatten = function () {
   return [].concat.apply([], this);
 };
+
+
+function lightmap(vertices, u, v, buffer) {
+  var s = [], t = [];
+  var count = vertices.length / 3;
+  for (var i = 0; i < count; i++) {
+    s.push(
+        vertices[i * 3 + 0] * u[0] +
+        vertices[i * 3 + 1] * u[1] +
+        vertices[i * 3 + 2] * u[2] +
+        u[3]);
+    t.push(
+        vertices[i * 3 + 0] * v[0] +
+        vertices[i * 3 + 1] * v[1] +
+        vertices[i * 3 + 2] * v[2] +
+        v[3]);
+  }
+  var width = Math.ceil(Math.max.apply(Math, s) - Math.min.apply(Math, s)) + 1;
+  var height = Math.ceil(Math.max.apply(Math, t) - Math.min.apply(Math, t)) + 1;
+  return lightmap2png(buffer, width, height);
+}
 
 
 module.exports = function (data, lightmaps) {
