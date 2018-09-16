ad305f6f896cd5782a01995a7bb0d2ac6e4adbee
71104 <a71104@gmail.com>
2018-09-16 15:46:03 +0100

    Don't create new objects every time an animation changes.

--- src/js/AnimatedModel.js
+++ src/js/AnimatedModel.js
@@ -1,6 +1,11 @@
 Quake2.AnimatedModel = function (model) {
   this._model = model;
-  this._animation = null;
+  this._animation = {
+    name: null,
+    firstFrame: 0,
+    lastFrame: 0,
+    startTime: 0,
+  };
 };
 
 Quake2.AnimatedModel.FRAME_DURATION = 100;
@@ -10,52 +15,54 @@ Quake2.AnimatedModel.prototype.setSkin = function (name) {
 };
 
 Quake2.AnimatedModel.prototype.play = function (name) {
-  this._animation = {
-    name: name,
-    firstFrame: 0,
-    lastFrame: this._model.animations[name].length - 1,
-    startTime: Date.now()
-  };
+  this._animation.name = name;
+  this._animation.firstFrame = 0;
+  this._animation.lastFrame = this._model.animations[name].length - 1;
+  this._animation.startTime = Date.now();
 };
 
 Quake2.AnimatedModel.prototype.playFrames = function (name, first, last) {
-  this._animation = {
-    name: name,
-    firstFrame: first,
-    lastFrame: last,
-    startTime: Date.now(),
-  };
+  this._animation.name = name;
+  this._animation.firstFrame = first;
+  this._animation.lastFrame = last;
+  this._animation.start_time = Date.now();
 };
 
 Quake2.AnimatedModel.prototype.playRandom = function (name, first, last) {
-  this._animation = {
-    name: name,
-    firstFrame: first,
-    lastFrame: last,
-    startTime: Math.round(Math.random() * (last - first + 1) * Quake2.AnimatedModel.FRAME_DURATION),
-  };
+  this._animation.name = name;
+  this._animation.firstFrame = first;
+  this._animation.lastFrame = last;
+  this._animation.startTime = Math.round(Math.random() * (last - first + 1) * Quake2.AnimatedModel.FRAME_DURATION);
 };
 
 Quake2.AnimatedModel.prototype.isRestarting = function (t) {
-  const elapsed = t - this._animation.startTime;
-  const frameNumber = Math.floor(elapsed / Quake2.AnimatedModel.FRAME_DURATION);
-  const frameCount = this._animation.lastFrame - this._animation.firstFrame + 1;
-  return frameNumber >= frameCount && !(frameNumber % frameCount);
+  if (this._animation.name) {
+    const elapsed = t - this._animation.startTime;
+    const frameNumber = Math.floor(elapsed / Quake2.AnimatedModel.FRAME_DURATION);
+    const frameCount = this._animation.lastFrame - this._animation.firstFrame + 1;
+    return frameNumber >= frameCount && !(frameNumber % frameCount);
+  } else {
+    return false;
+  }
 };
 
 Quake2.AnimatedModel.prototype.isAtFrame = function (t, i) {
-  const elapsed = t - this._animation.startTime;
-  const frameNumber = Math.floor(elapsed / Quake2.AnimatedModel.FRAME_DURATION);
-  const frameCount = this._animation.lastFrame - this._animation.firstFrame + 1;
-  return frameNumber % frameCount === i;
+  if (this._animation.name) {
+    const elapsed = t - this._animation.startTime;
+    const frameNumber = Math.floor(elapsed / Quake2.AnimatedModel.FRAME_DURATION);
+    const frameCount = this._animation.lastFrame - this._animation.firstFrame + 1;
+    return frameNumber % frameCount === i;
+  } else {
+    return false;
+  }
 };
 
 Quake2.AnimatedModel.prototype.stop = function () {
-  this._animation = null;
+  this._animation.name = null;
 };
 
 Quake2.AnimatedModel.prototype.render = function (x, y, z, a, t) {
-  if (this._animation) {
+  if (this._animation.name) {
     const frameTable = this._model.animations[this._animation.name];
     const elapsed = t - this._animation.startTime;
     const frameNumber = Math.floor(elapsed / Quake2.AnimatedModel.FRAME_DURATION);
