129390f9f2617e739566b2598e48beb9ff5011e1
Alberto La Rocca <a71104@gmail.com>
2019-07-04 00:05:31 +0100

    Sound pools

--- src/js/Sound.js
+++ src/js/Sound.js
@@ -1,9 +1,31 @@
 Quake2.Sound = function () {};
 
+Quake2.Sound.Pool = function (sound) {
+  sound.addEventListener('ended', Quake2.Sound.Pool._resetSound);
+  this._sounds = [sound];
+};
+
+Quake2.Sound.Pool._resetSound = function (event) {
+  event.target.currentTime = 0;
+};
+
+Quake2.Sound.Pool.prototype.play = function () {
+  for (var i = 0; i < this._sounds.length; i++) {
+    if (!this._sounds[i].currentTime) {
+      this._sounds[i].play();
+      return;
+    }
+  }
+  this._sounds.push(this._sounds[0].cloneNode(false));
+  this._sounds[this._sounds.length - 1].play();
+};
+
 Quake2.Sound._sounds = Object.create(null);
 
 Quake2.Sound.initialize = function (sounds) {
-  Quake2.Sound._sounds = sounds;
+  for (var key in sounds) {
+    Quake2.Sound._sounds[key] = new Quake2.Sound.Pool(sounds[key]);
+  }
 };
 
 Quake2.Sound.play = function (name) {
